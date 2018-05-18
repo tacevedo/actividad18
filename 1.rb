684ece260f78487dfd7cd158a1155a4f3668960a
unknown <tamara.acevedoavila@gmail.com>
2018-05-18 12:31:58 -0400

    primero ok

--- 1.rb
+++ 1.rb
@@ -4,7 +4,6 @@ class Table
     @name = nombre
     @recaudaciones = recaudaciones.map(&:to_i)
   end
-
 end
 
 def lee_archivo(archivo)
@@ -16,6 +15,20 @@ def lee_archivo(archivo)
     lista_recaudaciones << Table.new(*ls)
   end
   lista_recaudaciones
+end
+
+def maximo_dia(mesas)
+  mesas.each do |mesa|
+    aux = 0
+    dia = 0
+    mesa.recaudaciones.each_with_index do |dato, i|
+      if dato > aux
+        aux = dato
+        dia = i + 1
+      end
+    end
+    puts "mesa #{mesa.name} el dia #{dia} mas recaudaciones: #{aux}"
+  end
 end
 
 def promedio(mesas)
@@ -32,6 +45,12 @@ def promedio(mesas)
   return promedio = suma / (mesas.length.to_f * dias)
 end
 
+#Crea objetos Table
 objetos_mesa = lee_archivo('casino.txt')
+
+#mayor valor recaudado por mesa
+maximo_dia(objetos_mesa)
+
+#promedio de todos los dias todas las mesas
 print "promedio todas las mesas todos los dias: "
 puts promedio(objetos_mesa)
