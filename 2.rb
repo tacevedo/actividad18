b00f4fe08f00c4fb06fd30b8375031e8dbf0cdcf
unknown <tamara.acevedoavila@gmail.com>
2018-05-18 13:29:07 -0400

    avance 2

--- 2.rb
+++ 2.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Course
   attr_reader :name, :inicio, :termino
   def initialize(nombre, inicio, termino)
@@ -17,12 +18,36 @@ def lee_archivo(archivo)
   end
   lista_cursos
 end
+#curso inicia antes de
+def before_date(cursos, fecha = Date.today)
+  cursos_filter = []
+  cursos.each do |curso|
+    if Date.parse(curso.inicio) < fecha
+      cursos_filter.push(curso.name)
+    end
+  end
+  cursos_filter
+end
 
+#curso finaliza despues de
+def after_date(cursos, fecha = Date.today)
+  cursos_filter = []
+  cursos.each do |curso|
+    if Date.parse(curso.termino) < fecha
+      cursos_filter.push(curso.name)
+    end
+  end
+  cursos_filter
+end
 #Crea objetos Course
-objetos_curso = lee_archivo('cursos.txt')
-puts objetos_curso
+objetos_cursos = lee_archivo('cursos.txt')
 
 #cursos antes de una fecha
-
+fecha1 = "2017-05-17";
+puts "cursos que inician antes de #{fecha1}:"
+puts before_date(objetos_cursos, Date.parse(fecha1))
 
 #cursos despues de una fecha
+fecha2 = "2017-08-13";
+puts "cursos que terminan después de #{fecha2}:"
+puts after_date(objetos_cursos, Date.parse(fecha2))
