b2b52de4f307339d9ec2958743c57e728303308d
tam <tamara.acevedoavila@gmail.com>
2018-05-18 21:00:03 -0400

    algo

--- 2.rb
+++ 2.rb
@@ -23,7 +23,7 @@ end
 def before_date(cursos, fecha = Date.today)
   cursos_filter = []
   # >= 2018-01-01.
-  raise ArgumentError.new("Fecha inválida, debe ser menor a: 2018-01-01.") if fecha >= Date.parse("2019-01-01")
+  raise ArgumentError.new("Fecha inválida, debe ser menor a: 2019-01-01.") if fecha >= Date.parse("2019-01-01")
   cursos.each do |curso|
     if Date.parse(curso.inicio) < fecha
       cursos_filter.push(curso.name)
@@ -35,7 +35,7 @@ end
 
 #curso finaliza despues de
 def after_date(cursos, fecha = Date.today)
-  raise ArgumentError.new("Fecha inválida, debe ser mayor a: 2018-01-01.") if fecha >= Date.parse("2019-01-01")
+  raise ArgumentError.new("Fecha inválida, debe ser mayor a: 2019-01-01.") if fecha >= Date.parse("2019-01-01")
   cursos_filter = []
   cursos.each do |curso|
     if Date.parse(curso.termino) > fecha
@@ -51,13 +51,17 @@ objetos_cursos = lee_archivo('cursos.txt')
 #cursos antes de una fecha
 puts "ingrese una fecha para evaluar cursos que inician antes a esta (formato 2010-02-31)"
 fecha1 = gets.chomp
-# fecha1 = "2017-05-17";
-puts "cursos que inician antes de #{fecha1}:"
-puts before_date(objetos_cursos, Date.parse(fecha1))
+if fecha1 == ''
+  fecha1 = Date.today
+else
+  fecha1 = Date.parse(fecha1)
+end
+puts "cursos que inician antes de"
+puts before_date(objetos_cursos, fecha1)
 
 #cursos despues de una fecha
 puts "ingrese una fecha para evaluar cursos que terminen posterior a esta (formato 2010-02-31)"
 fecha2 = gets.chomp
 # fecha2 = "2018-08-13";
-puts "cursos que terminan después de #{fecha2}:"
+puts "cursos que terminan después de:"
 puts after_date(objetos_cursos, Date.parse(fecha2))
